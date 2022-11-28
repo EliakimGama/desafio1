aeeba7c516025140241c5c10a9120d138a4a7aa8
EliakimGama <eliakimgama@hotmail.com>
2022-11-27 21:42:48 -0300

    atualizando...

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+   
+	var btnMenuMobile = $('.fa-bars');
+
+	$('.btn-menumobile').on('click', function() {
+
+		$('.nav-container ul').toggleClass('open');
+
+	});
+
+});
